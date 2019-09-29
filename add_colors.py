61824b926f535bde44bec15e2dadfa337b7cd80d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-30 01:05:11 +0200

    Add files via upload

---
+++ add_colors.py
@@ -0,0 +1,70 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+img = cv2.imread("ok.png", 0)
+
+blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
+blank_image[0:img.shape[0], 0:img.shape[1]] = 0, 0, 0
+
+
+_, thresh = cv2.threshold(img, 24, 255, 0)
+
+
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE,
+                               cv2.CHAIN_APPROX_SIMPLE)
+
+
+liste = []
+for x in range(thresh.shape[0]):
+    for y in range(thresh.shape[1]):
+        
+        blank_image[x, y] = thresh[x, y]
+        
+        if thresh[x, y] == 255:
+            print(x, y)
+
+            blank_image[x, y] = 0, 0, 255
+
+            cv2.imshow("blank_image.png", blank_image)
+            cv2.imshow("thresh.png", thresh)
+            cv2.waitKey(0)
+
+
+
+
+
+
+
+##for x in range(thresh.shape[0]):
+##    for y in range(thresh.shape[1]):
+##    ##im = Image.open("ok.png")
+##    ##im.rotate(90).show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
