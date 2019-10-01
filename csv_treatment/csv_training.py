78d5b179d95e10a79a1ec7a9192d7308e10bf658
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-01 17:17:44 +0200

    Update csv_training.py

--- csv_treatment/csv_training.py
+++ csv_treatment/csv_training.py
@@ -9,7 +9,7 @@ from sklearn.utils import shuffle
 
 
 
-file =  open('csv/test.csv', 'r')
+file =  open('../csv/test.csv', 'r')
     
 dataframe = file.readlines()
 dataframe = dataframe[1:]
@@ -43,7 +43,7 @@ X_test,Y_test = X, Y
 
 model = svm.SVC(kernel="linear",C=2)
 model.fit(X_train,Y_train)
-joblib.dump(model, "models/captchat_recognition") 
+joblib.dump(model, "../models/captchat_recognition") 
 predictions = model.predict(X_test)
 
 print("Score", metrics.accuracy_score(Y_test, predictions))
